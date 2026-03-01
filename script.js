1bc965da9f0640a085d5eb76f3b0f73046e55959
AkashAdak-coder <akashadak2608@gmail.com>
2026-03-01 16:52:17 +0530

    delete gameplay function

--- script.js
+++ script.js
@@ -4,60 +4,6 @@ let score = JSON.parse(localStorage.getItem('score')) || {
             draw : 0
         }
         
-        function gamePlay(user_move){
-            let compter_move = computerMove();
-            let output = document.querySelector('.play-output');
-            let display_result = document.querySelector('.display-result');
-
-            if(user_move === 'rock'){
-                if(compter_move === 'rock'){
-                    // alert(`You choose ${user_move}, Computer choose ${compter_move}, it's draw!`);
-                    output.innerText = "It's Draw!";
-                    output.classList.add("play-output");
-                } else if (compter_move === 'paper') {
-                    // alert(`You choose ${user_move}, Computer choose ${compter_move}, compter won`);
-                    output.innerText = "You Lose";
-                    output.classList.add("play-output");
-                } else {
-                    // alert(`You choose ${user_move}, Computer choose ${compter_move}, you win`);
-                    output.innerText = "You Win";
-                    output.classList.add("play-output");
-                }
-                ShowResult(user_move,compter_move);
-                display_result.classList.add("display-result-id");
-            }
-
-            if(user_move === 'paper'){
-                if(compter_move === 'rock'){
-                    // alert(`You choose ${user_move}, Computer choose ${compter_move}, you win`);
-                    output.innerText = "You Win";
-                } else if (compter_move === 'paper') {
-                    // alert(`You choose ${user_move}, Computer choose ${compter_move}, it draw!`);
-                    output.innerText = "It's Draw!";
-                } else {
-                    // alert(`You choose ${user_move}, Computer choose ${compter_move}, computer win`);
-                    output.innerText = "You Lose";
-                }
-                ShowResult(user_move,compter_move);
-                display_result.classList.add("display-result-id");
-            }
-
-            if(user_move === 'scissor'){
-                if(compter_move === 'rock'){
-                    // alert(`You choose ${user_move}, Computer choose ${compter_move}, computer win`);
-                    output.innerText = "You Lose";
-                } else if (compter_move === 'paper') {
-                    // alert(`You choose ${user_move}, Computer choose ${compter_move}, you win`);
-                    output.innerText = "You Win";
-                } else {
-                    // alert(`You choose ${user_move}, Computer choose ${compter_move}, it's draw!`);
-                    output.innerText = "It's Draw!";
-                }
-                ShowResult(user_move,compter_move);
-                display_result.classList.add("display-result-id");
-            }
-
-        }
         function computerMove(){
             let n = Math.floor((Math.random() * 3)) + 1;
             let pick = '';
@@ -87,7 +33,7 @@ let score = JSON.parse(localStorage.getItem('score')) || {
             localStorage.removeItem('score');
         }
 
-        function ShowResult(user_move ,compter_move) {
+        function countWinning(user_move ,compter_move) {
             let spans = document.getElementsByTagName('span');
             
             if(compter_move === user_move){
