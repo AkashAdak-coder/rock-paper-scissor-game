7552f9d2318edf4509854df882d38355b2b6880e
AkashAdak-coder <akashadak2608@gmail.com>
2026-08-01 17:33:59 +0530

    add feature showMove and displayResult

--- script.js
+++ script.js
@@ -6,27 +6,31 @@ const scores = JSON.parse(localStorage.getItem('score')) || {
 };
 
 function game(userMove){
+
     const count = Math.floor(Math.random() * 3);
     const move = ['rock','paper','scissor'];
     const computerMove = move[count];
+
     if((userMove === 'rock' &&  computerMove === 'paper')|| (userMove === 'scissor' && computerMove === 'rock') || (userMove === 'paper' && computerMove === 'scissor')){
-    result = "lose";
+    result = "Lose";
     scores.lose += 1;
-    alert(`Computer choose ${computerMove}, User choose ${userMove}, User ${result}
+    alert(`User choose ${userMove}, Computer choose ${computerMove}, It's ${result}
 win : ${scores.win}  lose : ${scores.lose}  tie : ${scores.tie}`);
     } else if((userMove === 'rock' && computerMove === 'scissor')|| (userMove === 'scissor' && computerMove === 'paper') || (userMove === 'paper' && computerMove === 'rock')){
-    result = 'win';
+    result = 'Win';
     scores.win += 1;
-    alert(`Computer choose ${computerMove}, User choose ${userMove}, User ${result}
+    alert(`User choose ${userMove}, Computer choose ${computerMove}, It's ${result}
 win : ${scores.win}  lose : ${scores.lose}  tie : ${scores.tie}`);
     } else {
     result = 'Tie';
     scores.tie += 1;
-    alert(`Computer choose ${computerMove}, User choose ${userMove}, It's ${result}
+    alert(`User choose ${userMove}, Computer choose ${computerMove}, It's ${result}
 win : ${scores.win}  lose : ${scores.lose}  tie : ${scores.tie}`);
     }
 
     localStorage.setItem('score',JSON.stringify(scores));
+    displayResult(result);
+    showMoves(userMove,computerMove);
 }
 
 // function pickComputerMove(count){
@@ -45,4 +49,48 @@ function resetScore(){
     scores.tie = 0;
     localStorage.removeItem('score');
     alert('score has been reset!');
+}
+
+function displayResult(result){
+    const displayResult = document.querySelector('.js-result');
+
+    displayResult.textContent = `${result}`;
+}
+
+function showMoves(move1,move2){
+    const showMove = document.querySelector('.js-show-moves');
+
+    if (move1 === 'rock' && move2 === 'paper'){
+        showMove.innerHTML = `
+            <i class="fa-regular fa-hand-back-fist"></i>
+            <i class="fa-regular fa-hand"></i>
+        `;
+    } else if(move1 === 'rock' && move2 === 'scissor'){
+        showMove.innerHTML = `
+            <i class="fa-regular fa-hand-back-fist"></i>
+            <i class="fa-regular fa-hand-scissors"></i>
+        `;
+    }else if(move1 === 'paper' && move2 === 'scissor'){
+        showMove.innerHTML = `
+            <i class="fa-regular fa-hand"></i>
+            <i class="fa-regular fa-hand-scissors"></i>
+        `;
+    }else if(move1 === 'paper' && move2 === 'rock'){
+        showMove.innerHTML = `
+            <i class="fa-regular fa-hand"></i>
+            <i class="fa-regular fa-hand-back-fist"></i>
+        `;
+    }else if(move1 === 'scissor' && move2 === 'rock'){
+        showMove.innerHTML = `
+            <i class="fa-regular fa-hand-scissors"></i>
+            <i class="fa-regular fa-hand-back-fist"></i>
+        `;
+    }else{
+        showMove.innerHTML = `
+            <i class="fa-regular fa-hand-scissors"></i>
+            <i class="fa-regular fa-hand"></i>
+        `;
+    }
+       console.log(move1, move2);
+
 }
